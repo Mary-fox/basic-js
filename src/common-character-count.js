7453f8681486a0b41a932633948948e748b2b234
Mary <89971822+Mary-fox@users.noreply.github.com>
2022-10-15 16:20:55 +0300

    feat : common-character-count done

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,9 +11,22 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
+function getCommonCharacterCount(s1, s2) {
+//throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let count = 0;
+  const num = s2.split("");
+  for(str of s1){
+    let index = num.findIndex(s => s === str);
+    if(index >= 0){
+      count++;
+      num.splice(index, 1);
+      //  удаляем символ из массива символов второй входной строки,
+      // чтобы он не учитывался при следующем совпадении:
+    }
+  }
+  return count;
+
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -11,11 +11,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
+function getSeason(date) {
+  //throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let season = 0;
+if (!(date instanceof Date)) {
+  return new Error("Invalid date!");
+} else if (date == undefined) { return "Unable to determine the time of year!";
 }
-
+switch (date.getMonth() + 1) {
+  case (12):
+  case (1):
+  case (2):
+    return 'winter';
+  case (3):
+  case (4):
+  case (5):
+    return 'spring';
+  case (6):
+  case (7):
+  case (8):
+    return 'summer';
+  case (9):
+  case (10):
+  case (11):
+    return 'autumn (fall)';
+}
+}
 module.exports = {
   getSeason
 };
